0c042c9ee51cbb4578f9e436c611255bd53f8344
Tim <108850507+timtung123@users.noreply.github.com>
2022-10-20 19:23:50 +0800

    Add files via upload

---
+++ src/navbar.js
@@ -0,0 +1,14 @@
+export default function Navbar(){
+return<nav className="nav">
+    <a href="/" className="site-title">site Name</a>
+<ul>
+    <li>
+<a href="/pricing">pricing</a>
+</li>
+<li>
+<a href="/about">about</a>
+    </li>
+</ul>
+</nav>
+
+}
